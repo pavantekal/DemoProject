c95c5088478e5265ac4808e6821395da4d0c7d5b
Pavan <tekal.pavan@gmail.com>
2020-11-05 20:36:23 +0530

    added circleci

---
+++ DemoProject/src/com/demoProject/ExampleClass.java
@@ -0,0 +1,8 @@
+package com.demoProject;
+
+public class ExampleClass {
+
+    public static void main(String[] args) {
+	System.out.println("This is a demo program, on java");
+    }
+}
--- DemoProject/src/com/demoProject/Main.java
+++
@@ -1,8 +0,0 @@
-package com.demoProject;
-
-public class Main {
-
-    public static void main(String[] args) {
-	System.out.println("This is a demo progra, on java");
-    }
-}
